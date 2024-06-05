90c30b7f305d50649fdc14da8250427836f1097e
husen <husen@zicure.com>
2024-06-05 18:23:36 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MyComponent = () => (
+  <View>
+    <Text>Hello from my React Native package!</Text>
+  </View>
+);
+
+export default MyComponent;
